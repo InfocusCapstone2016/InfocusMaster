000cd4b647a8a2f2d795c00a543331f5199e5740
net5033 <ntorretti7391@gmail.com>
2016-04-18 15:17:08 -0400

    Working Main Slider
    Working Main Slider

--- assets/scripts/homePage.js
+++ assets/scripts/homePage.js
@@ -20,27 +20,13 @@ $(window).load(function() { //start after HTML, images have loaded
             //loop through the items
             var infiniteLoop = setInterval(function(){
 				$('.program-rotate').eq(currentItem).hide( "fade", "slow" );
-						
-				
-				
                 if(currentItem == numberOfItems-1){		 
-				
                     currentItem = 0;
 					$('.program-rotate').eq(currentItem).show("fade", "slow");
-					
-					
-
                 }else{
-			
 						currentItem++;
 						$('.program-rotate').eq(currentItem).show("fade", "slow");
-						
-
-                }
-			
-
-				
-               
+				}
             }, itemInterval);
         }
     };
@@ -56,21 +42,16 @@ $(window).load(function() { //start after HTML, images have loaded
             var testItemLength = $('.testimonial-rotate').length;
             //set current item
             var testimonialItem = 0;
-           
             //loop through the items
             var infiniteTestLoop = setInterval(function(){
-				
-            
                 if(testimonialItem == testItemLength-1){
 					 $('.testimonial-rotate').eq(testimonialItem).hide();
                     testimonialItem = 0;
 					$('.testimonial-rotate').eq(testimonialItem).effect("slide", "slow");
-					
                 }else{
 					 $('.testimonial-rotate').eq(testimonialItem).hide();
                     testimonialItem++;
 					$('.testimonial-rotate').eq(testimonialItem).effect("slide", "slow");
-					
                 }
               
             }, testInterval);
@@ -79,47 +60,11 @@ $(window).load(function() { //start after HTML, images have loaded
     testimonialRotator.init();
 	
 	
-    setInterval(function () {
-        moveRight();
-    }, 3000);
- 
-  
-	var slideCount = $('#slider ul li').length;
-	var slideWidth = $('#slider ul li').width();
-	var slideHeight = $('#slider ul li').height();
-	var sliderUlWidth = slideCount * slideWidth;
-	
-	$('#slider').css({ width: slideWidth, height: slideHeight });
-	
-	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-	
-    $('#slider ul li:last-child').prependTo('#slider ul');
-
-    function moveLeft() {
-        $('#slider ul').animate({
-            left: + slideWidth
-        }, 2000, function () {
-            $('#slider ul li:last-child').prependTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
-
-    function moveRight() {
-        $('#slider ul').animate({
-            left: - slideWidth
-        }, 2000, function () {
-            $('#slider ul li:first-child').appendTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
-
-    $('a.control_prev').click(function () {
-        moveLeft();
-    });
-
-    $('a.control_next').click(function () {
-        moveRight();
-    });
+	//main slider
+	$('.bxslider').bxSlider({
+  		auto: true,
+  		autoControls: false
+	});
 
 });    
 
