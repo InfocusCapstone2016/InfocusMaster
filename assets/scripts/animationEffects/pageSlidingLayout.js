585d877e29bfdb605170c6d9b6ef598f7f58986e
net5033 <ntorretti7391@gmail.com>
2016-04-22 10:13:34 -0400

    Creating a function for the sliding page layout animation

---
+++ assets/scripts/animationEffects/pageSlidingLayout.js
@@ -0,0 +1,20 @@
+var slidingContent=function (){
+	//hiding content except for overview
+	$('.trContent').hide();
+	$('#overview').show();
+	
+	//getting the header that was clicked on
+	$('.trHeader').click(function(){
+		//getting the id of the header
+		header=$(this).attr('id');
+		//looping through each header to slide up the ones that were not clicked on
+		$('.trHeader').each(function(){
+			if($(this).attr('id') !== header){
+				$(this).next('.trContent').slideUp('slow');
+			}
+		});
+		//sliding downt the content of the header that was clicked on
+		$(this).next('.trContent').slideToggle('slow');
+	});
+	
+}
