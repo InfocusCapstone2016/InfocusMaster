64f2b027af5a3cc52b6bf7a6d659ef4bbee95346
net5033 <ntorretti7391@gmail.com>
2016-05-18 11:22:33 -0400

    Added in the content click API for the student spotlight
    -Natalie

--- assets/scripts/APIs/contentClick.js
+++ assets/scripts/APIs/contentClick.js
@@ -1,4 +1,15 @@
-$(document.ready(function(){
-SignageInterface.contentClick('http://www.infocus.com');
-
+$('#jeffLink').click(function(){
+	SignageInterface.contentClick('https://www.linkedin.com/in/jeffrey-lanning-37b8b1120');
+});
+$('#natalieLink').click(function(){
+	SignageInterface.contentClick('https://www.linkedin.com/in/natalie-torretti-24755486');
+});
+$('#bryanLink').click(function(){
+	SignageInterface.contentClick('https://www.linkedin.com/in/bryan-butler-279827116');
+});
+$('#jimLink').click(function(){
+	SignageInterface.contentClick('https://www.linkedin.com/in/jdmcelwain');
+});
+$('#coleLink').click(function(){
+	SignageInterface.contentClick('https://www.linkedin.com/in/cole-brungart-a7628211b');
 });
--- assets/scripts/animationEffects/homePageAnimation.js
+++ assets/scripts/animationEffects/homePageAnimation.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
 	FadeEffect.init();
 	longerFadeEffect.init();
-	
-	$('#studentSpotlight').click(function(){
-		SignageInterface.contentClick('http://www.google.com');
-		
-	});
 	 
 });
